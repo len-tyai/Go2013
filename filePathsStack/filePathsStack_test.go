eda191404b05f2afe5f79004c85f0157fa4a414d
len-tyai <antinomi87@gmail.com>
2014-11-12 15:24:23 +0100

    TEST 4

--- filePathsStack/filePathsStack_test.go
+++ filePathsStack/filePathsStack_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestFirstParsePath(t *testing.T) {
-	path := "D/go/coding/../source/warcluster/tests/first/../../"
+	path := "D/go/coding/../source/MYTH/tests/first/../../"
 
 	if parsePath(path) != "/D/go/source/warcluster/" {
 		t.Error("Result path is ", parsePath(path))
